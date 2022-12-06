eb0433241325e8e25ee41b045340bda9f687c457
Jonathan <jonathan.hedlund.371@gmail.com>
2022-12-06 07:33:24 +0100

    Completed day 6 and restructured map

---
+++ 2022/6-tuning-trouble/6-tuning-trouble.js
@@ -0,0 +1,34 @@
+const { readFileSync } = require('fs')
+
+const file = readFileSync('./input.txt', 'utf-8').split('')
+
+const twoOfSame = function (buffer) {
+    let bool = false
+    buffer.forEach((el, ind) => {
+        buffer.find((ele, inde) => {
+            if (ind !== inde && el === ele) bool = true
+        })
+    })
+    return bool
+}
+
+const calcFirstUniqueString = function (messageLength, file) {
+    const buffer = []
+    for (i = 1; i <= file.length; i++) {
+        if (buffer.length > messageLength) {
+            buffer.shift()
+            buffer.push(file[i])
+        } else {
+            buffer.push(file[i])
+        }
+
+        if (!twoOfSame(buffer) && i > 4) {
+            return i
+        }
+    }
+}
+
+const packetMarker = calcFirstUniqueString(4, file)
+const messageMarker = calcFirstUniqueString(14, file)
+
+console.log(packetMarker, messageMarker)
