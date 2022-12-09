4eac108098a13623892a32a7d6829b19e6465861
Jonathan <jonathan.hedlund.371@gmail.com>
2022-12-09 23:54:09 +0100

    Improvements to day 9 AOC 2022

--- 2022/9-rope-bridge/9-rope-bridge.js
+++ 2022/9-rope-bridge/9-rope-bridge.js
@@ -10,26 +10,18 @@ let knots = new Array(amountOfPoints).fill('0 0')
 
 const tailFinalPos = []
 
-const moveHead = function (knots, dir) {
-    let [curX, curY] = knots[0].split(' ')
+const moveHead = function (curPos, dir) {
+    let [curX, curY] = curPos.split(' ')
 
-    if (dir === 'R') {
-        knots[0] = `${++curX} ${curY}`
-    }
-    if (dir === 'L') {
-        knots[0] = `${--curX} ${curY}`
-    }
-    if (dir === 'U') {
-        knots[0] = `${curX} ${++curY}`
-    }
-    if (dir === 'D') {
-        knots[0] = `${curX} ${--curY}`
-    }
+    if (dir === 'R') return `${++curX} ${curY}`
+    if (dir === 'L') return `${--curX} ${curY}`
+    if (dir === 'U') return `${curX} ${++curY}`
+    if (dir === 'D') return `${curX} ${--curY}`
 }
 
-const moveTail = function (hCur, tCur) {
-    let [headX, headY] = hCur.split(' ').map((el) => parseInt(el))
-    let [tailX, tailY] = tCur.split(' ').map((el) => parseInt(el))
+const moveTail = function (headPos, tailPos) {
+    let [headX, headY] = headPos.split(' ').map((el) => parseInt(el))
+    let [tailX, tailY] = tailPos.split(' ').map((el) => parseInt(el))
 
     if (Math.abs(tailX - headX) > 1 || Math.abs(tailY - headY) > 1) {
         const diffX = headX - tailX
@@ -46,7 +38,7 @@ input.forEach((move) => {
     const [dir, steps] = move.split(' ')
 
     for (let step = 0; step < parseInt(steps); step++) {
-        moveHead(knots, dir)
+        knots[0] = moveHead(knots[0], dir)
 
         for (let i = 0; i < knots.length; i++) {
             if (i !== knots.length - 1) {
@@ -60,5 +52,4 @@ input.forEach((move) => {
 
 const uniquePositions = new Set(tailFinalPos)
 
-console.log(uniquePositions)
 console.log(uniquePositions.size)
